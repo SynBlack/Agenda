cc2e1af72abf04939436bd4eaf0a971e1e1b28c7
Darkcrow <Darkcrow@192.168.1.102>
2018-12-08 10:37:15 +0100

    Creación de los métodos get

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -5,7 +5,7 @@ package org.iesalandalus.programacion.agenda;
  *
  */
 public class Agenda {
-<<<<<<< HEAD
+
 	private static final int MAX_CONTACTOS = 100;
 	private int numContactos;
 	private Contacto[] contactos = null;
@@ -20,9 +20,5 @@ public class Agenda {
 
 	}
 
-=======
-	private static final String ERROR;
-	private static final String EXITO;
->>>>>>> refs/remotes/origin/master
 	
 }
