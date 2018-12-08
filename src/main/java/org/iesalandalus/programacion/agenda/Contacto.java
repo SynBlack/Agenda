f4bd28dd2a136d21b17cbcf943f8d77f00a0aaa4
Darkcrow <Darkcrow@192.168.1.102>
2018-12-08 10:15:41 +0100

    Añadimos el constructor con tres parámetros String de nombre, teléfono y correo.

--- src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -14,82 +14,96 @@ public class Contacto {
 	private String telefono;
 	private String correo;
 
+	public Contacto(String nombre, String telefono, String correo) {
+
+		this.setNombre(nombre);
+		this.setTelefono(telefono);
+		this.setCorreo(correo);
+	}
+
 	public String getNombre() {
-        return this.nombre;
-    }
-     private void setNombre(String nombre) {
-        boolean creado = false;
-        if (this.nombre != null) {
-            creado = true;
-        }
-        if (nombre != null && nombre != "" && creado != true) {
-            this.nombre = nombre;
-        } else {
-            throw new IllegalArgumentException("El nombre de un contacto no puede ser nulo o vacío.");
-        }
-    }
-     public String getTelefono() {
-        return this.telefono;
-    }
-     public void setTelefono(String telefono) {
-         if (telefono != null && telefono != "") {
-            if (Pattern.matches(ER_TELEFONO, telefono)) {
-                this.telefono = telefono;
-            } else {
-                throw new IllegalArgumentException("El teléfono no tiene un formato válido.");
-            }
-        } else {
-            throw new IllegalArgumentException("El teléfono de un contacto no puede ser nulo o vacío.");
-        }
-    }
-     public String getCorreo() {
-        return this.correo;
-    }
-     public void setCorreo(String correo) {
-         if (correo != null && correo != "") {
-            if (Pattern.matches(ER_CORREO, correo)) {
-                this.correo = correo;
-            } else {
-                throw new IllegalArgumentException("El correo no tiene un formato válido.");
-            }
-        } else {
-            throw new IllegalArgumentException("El correo de un contacto no puede ser nulo o vacío.");
-        }
-    }
-     
-     @Override
-     public String toString() {
-          return nombre + " [" + telefono + ", " + correo + "]";
-     }
-      @Override
-     public int hashCode() {
-         int hash = 7;
-         hash = 61 * hash + Objects.hashCode(this.nombre);
-         hash = 61 * hash + Objects.hashCode(this.telefono);
-         hash = 61 * hash + Objects.hashCode(this.correo);
-         return hash;
-     }
-      @Override
-     public boolean equals(Object obj) {
-         if (this == obj) {
-             return true;
-         }
-         if (obj == null) {
-             return false;
-         }
-         if (getClass() != obj.getClass()) {
-             return false;
-         }
-         final Contacto other = (Contacto) obj;
-         if (!Objects.equals(this.nombre, other.nombre)) {
-             return false;
-         }
-         if (!Objects.equals(this.telefono, other.telefono)) {
-             return false;
-         }
-         if (!Objects.equals(this.correo, other.correo)) {
-             return false;
-         }
-         return true;
-     }
+		return this.nombre;
+	}
+
+	private void setNombre(String nombre) {
+		boolean creado = false;
+		if (this.nombre != null) {
+			creado = true;
+		}
+		if (nombre != null && nombre != "" && creado != true) {
+			this.nombre = nombre;
+		} else {
+			throw new IllegalArgumentException("El nombre de un contacto no puede ser nulo o vacío.");
+		}
+	}
+
+	public String getTelefono() {
+		return this.telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		if (telefono != null && telefono != "") {
+			if (Pattern.matches(ER_TELEFONO, telefono)) {
+				this.telefono = telefono;
+			} else {
+				throw new IllegalArgumentException("El teléfono no tiene un formato válido.");
+			}
+		} else {
+			throw new IllegalArgumentException("El teléfono de un contacto no puede ser nulo o vacío.");
+		}
+	}
+
+	public String getCorreo() {
+		return this.correo;
+	}
+
+	public void setCorreo(String correo) {
+		if (correo != null && correo != "") {
+			if (Pattern.matches(ER_CORREO, correo)) {
+				this.correo = correo;
+			} else {
+				throw new IllegalArgumentException("El correo no tiene un formato válido.");
+			}
+		} else {
+			throw new IllegalArgumentException("El correo de un contacto no puede ser nulo o vacío.");
+		}
+	}
+
+	@Override
+	public String toString() {
+		return nombre + " [" + telefono + ", " + correo + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 61 * hash + Objects.hashCode(this.nombre);
+		hash = 61 * hash + Objects.hashCode(this.telefono);
+		hash = 61 * hash + Objects.hashCode(this.correo);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Contacto other = (Contacto) obj;
+		if (!Objects.equals(this.nombre, other.nombre)) {
+			return false;
+		}
+		if (!Objects.equals(this.telefono, other.telefono)) {
+			return false;
+		}
+		if (!Objects.equals(this.correo, other.correo)) {
+			return false;
+		}
+		return true;
+	}
 }
