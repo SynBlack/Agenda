720fea4199fa33c03af8ba8c9d50f1d56de7fd63
Darkcrow <Darkcrow@192.168.1.102>
2018-12-08 10:22:07 +0100

    Creación de la clase Agenda con sus atributos

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.agenda;
+
+/**
+ * @author Emanuel Martínez Lonardi
+ *
+ */
+public class Agenda {
+	private static final String ERROR;
+	private static final String EXITO;
+	
+}
