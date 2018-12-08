b8e6856878e79c9a83482c74f6423b76c1c0d84f
Darkcrow <Darkcrow@192.168.1.102>
2018-12-08 10:09:39 +0100

    Creación de la clase Contacto con sus atributos correspondientes

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,14 @@
+package org.iesalandalus.programacion.agenda;
+
+/**
+ * @author Emanuel Martínez Lonardi
+ *
+ */
+public class Contacto {
+	private static final String ER_TELEFONO;
+	private static final String ER_CORREO;
+    private String nombre;
+    private String telefono;
+    private String correo;
+
+}
